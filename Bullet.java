b45be534b47f0de5604c1560dd129c8c4a1cf275
sophiezym <SophieYM@wpa-4-1726.cc.umanitoba.ca>
2016-12-03 15:55:19 -0600

    .

--- Bullet.java
+++ Bullet.java
@@ -14,20 +14,21 @@ public class Bullet extends GameObject{
 	//....
 	//..
 	public Bullet(Player player1) {
-		super(player1.wx,player1.wy + player1.height/2+ height/2,width,height);
+		super(player1.wx,player1.wy,width,height);
 		this.player1 = player1;
-		vx = (float)Math.sin(Math.toRadians(player1.angle));
-		vy = (float)Math.cos(Math.toRadians(player1.angle));
 		this.angle = player1.angle;
+		vx = (float)Math.sin(Math.toRadians(-angle));
+		vy = (float)Math.cos(Math.toRadians(-angle));
+		
 	}
 	
 	public void draw(GL2 gl)
 	{
 		gl.glPushMatrix();
 		
-		gl.glTranslatef(player1.wx,player1.wy,0);
+		gl.glTranslatef(wx,wy,0);
 		gl.glRotated(angle, 0, 0, 1f);
-		gl.glTranslatef(-player1.wx,-player1.wy,0);
+		gl.glTranslatef(-wx,-wy,0);
 		
 		gl.glBegin(GL2.GL_POLYGON);
 		gl.glColor3f(1, 0.027f, 0.192f);
