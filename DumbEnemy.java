afa74e5a89ca5a3b03728fff85404b3fdcb26fc0
EnoshAnwar <enosh.anwar12@gmail.com>
2016-12-03 14:12:52 -0600

    .

--- DumbEnemy.java
+++ DumbEnemy.java
@@ -1,14 +1,22 @@
+import java.util.Random;
+
 import com.jogamp.opengl.GL2;
 
 public class DumbEnemy extends GameObject{
 	
 	private final static float width = 50, height = 50;
 	private float r, g, b;
+	private final int min = 0, max = 3;
+	
 	public DumbEnemy(float wx, float wy) {
+		
 		super(wx, wy, width, height);
 		r = (float) Math.random();
 		g = (float) Math.random();
 		b = (float) Math.random();
+
+	    vx = (float)Math.random()*(max - min) + min;
+	    vy = (float)Math.random()*(max - min) + min;
 	}
 
 	
