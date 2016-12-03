a9b04ca39814e58682ff7237520706ef4aaecf52
EnoshAnwar <enosh.anwar12@gmail.com>
2016-12-03 12:36:30 -0600

    .

--- DumbEnemy.java
+++ DumbEnemy.java
@@ -2,16 +2,19 @@ import com.jogamp.opengl.GL2;
 
 public class DumbEnemy extends GameObject{
 	
-	final static float width = 40, height = 40;
-	
+	private final static float width = 70, height = 70;
+	private float r, g, b;
 	public DumbEnemy(float wx, float wy) {
 		super(wx, wy, width, height);
+		r = (float) Math.random();
+		g = (float) Math.random();
+		b = (float) Math.random();
 	}
 
 	
 	public void draw(GL2 gl) {
-		gl.glColor3f(0, 1, 0);
-		gl.glBegin(GL2.GL_POLYGON);
+		gl.glColor3f(r, g, b);
+		gl.glBegin(GL2.GL_QUAD_STRIP);
 		gl.glVertex2f(wx-width/2,wy-height/2);
 		gl.glVertex2f(wx+width/2,wy-height/2);
 		gl.glVertex2f(wx-width/2,wy+height/2);
@@ -21,7 +24,8 @@ public class DumbEnemy extends GameObject{
 
 	
 	public void update() {
-		
+		wx += vx;
+		wy += vy;
 	}
 
 	
