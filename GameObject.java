a45a07d360eeca2f1d37a1382eaf546847c7e93b
DimaMukhin <dima9348@gmail.com>
2016-12-03 11:25:06 -0600

    .

--- GameObject.java
+++ GameObject.java
@@ -1,4 +1,12 @@
-
 public abstract class GameObject {
+	protected float wx, wy; // world position x and y
+	protected float vx, vy; // velocity x and velocity y
+	protected float width, height; // width and height of the object
 	
+	public GameObject(float wx, float wy, float width, float height) {
+		this.wx = wx;
+		this.wy = wy;
+		this.width = width;
+		this.height = height;
+	}
 }
