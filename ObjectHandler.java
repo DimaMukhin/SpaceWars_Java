1702f44a67a2082c15067ab23b700b9aa5194aa7
DimaMukhin <dima9348@gmail.com>
2016-12-03 12:08:44 -0600

    .

--- ObjectHandler.java
+++ ObjectHandler.java
@@ -9,6 +9,10 @@ public class ObjectHandler {
 		list = new LinkedList<GameObject>();
 	}
 	
+	public void add(GameObject newObject) {
+		list.add(newObject);
+	}
+	
 	public void draw(GL2 gl) {
 		for (int i = 0; i < list.size(); i++) {
 			list.get(i).draw(gl);
@@ -20,7 +24,27 @@ public class ObjectHandler {
 		for (int i = 0; i < list.size(); i++) {
 			curr = list.get(i);
 			
-			curr.update();
+			if (objectOutOfBounds(curr)) {
+				list.remove(curr);
+			} else {
+				curr.update();
+			}
 		}
+	}
+	
+	private boolean objectOutOfBounds(GameObject curr) {
+		boolean out = false;
+		
+		if (curr.wx + (curr.width / 2.0f) > SpaceWars.INITIAL_WIDTH) {
+			out = true;
+		} else if (curr.wx - (curr.width / 2.0f) < 0) {
+			out = true;
+		} else if (curr.wy + (curr.height / 2.0f) > SpaceWars.INITIAL_HEIGHT) {
+			out = true;
+		} else if (curr.wy - (curr.height / 2.0f) < 0) {
+			out = true;
+		}
+			
+		return out;
 	}
 }
