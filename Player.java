4ac4861c36bdf3c8d43755cf28f87586f5d3737b
sophiezym <SophieYM@wpa-4-1726.cc.umanitoba.ca>
2016-12-03 13:51:08 -0600

    .

--- Player.java
+++ Player.java
@@ -18,15 +18,15 @@ public class Player extends GameObject{
 			green = (float)Math.random();
 			blue = (float)Math.random();
 		}while(red <= 0.3f && green <= 0.3f && blue <=0.3f);
-//		red = 0.059f;
-//		green = 1;
-//		blue = 1;
+
 	}
 	
 	public void draw(GL2 gl)
 	{
 		gl.glPushMatrix();
+		gl.glTranslatef(wx,wy,0);
 		gl.glRotated(angle, 0, 0, 1f);
+		gl.glTranslatef(-wx,-wy,0);
 		gl.glBegin(GL2.GL_TRIANGLES);
 		gl.glColor3f(red, green, blue);
 		gl.glVertex2f(wx-height/2,wy-height/2);
