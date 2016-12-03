e23460cefbd8b68390f7a28fe8ba0c31efddf697
EnoshAnwar <enosh.anwar12@gmail.com>
2016-12-03 11:47:58 -0600

    .

---
+++ DumbEnemy.java
@@ -0,0 +1,12 @@
+import com.jogamp.opengl.GL2;
+
+public class DumbEnemy extends GameObject{
+
+	public DumbEnemy(float wx, float wy, float width, float height) {
+		super(wx, wy, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
